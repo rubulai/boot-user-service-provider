ad0d44c96ef05c64b152eb147870c9c23e8771ef
eclipse_user <eclipse_user@sina.com>
2018-12-06 10:23:26 +0800

    服務提供者

---
+++ src/main/java/com/bdm/gmall/BootUserServiceProviderApplication.java
@@ -0,0 +1,15 @@
+package com.bdm.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+
+@EnableDubbo
+@SpringBootApplication
+public class BootUserServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootUserServiceProviderApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bdm/gmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bdm.gmall.service.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.bdm.gmall.bean.UserAddress;
+import com.bdm.gmall.service.UserService;
+
+@Component
+@Service
+public class UserServiceImpl implements UserService {
+
+	public List<UserAddress> getUserAddressList(String userId) {
+		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
+		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
+		return Arrays.asList(address1, address2);
+	}
+}
